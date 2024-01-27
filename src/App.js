8cf3f1b05a649f373d969bec659225d6cb588b7d
chichobanche <kostadin.pejoski1@gmail.com>
2024-01-27 23:05:42 +0100

    setted up the navigation between pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-
+import HomePage from './Pages/HomePage'
+import LoginPage from './Pages/LoginPage'
+import CheckOutPage from './Pages/CheckoutPage'
+import ProductsPage from './Pages/ProductsPage';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
 
 function App() {
  
-  
+  return (
+        <BrowserRouter>
+          <Routes>
+            <Route element={<HomePage></HomePage>} path='/home'/>
+            <Route index element={<HomePage></HomePage>}/>
+            <Route element={<LoginPage></LoginPage>} path='/login'/>
+            <Route element={<CheckOutPage></CheckOutPage>} path='/checkout'/>
+            <Route element={<ProductsPage></ProductsPage>} path='products'/>
+          </Routes>
+        </BrowserRouter>
+  )
 }
 
 export default App;
